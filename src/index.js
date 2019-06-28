e0d98b2e9b871342e52ddbc5d9742a827c6bc882
yhc <wangchao@wangchaodeMacBook-Air.local>
2019-06-28 17:33:45 +0800

    暂存

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const glob = require('glob');
 const fs = require('fs');
+const args = process.argv.slice(2)
 function getHtml() {
     let arr = [];
     return new Promise((resolve,reject)=>{
@@ -15,9 +16,14 @@ getHtml().then(files=>{
     files.forEach(url => {
         let vtrHtml = fs.readFileSync(url, 'utf8')
         var rgx = /http(s?)\:\/\/ggtf\.sunland\.org\.cn\/statistics\/interactive\.js/g;
-        vtrHtml = vtrHtml.replace(rgx,'');
+        var regs = /http(s?)\:\/\/ggtf\.sunland\.org\.cn\/statistics\/sunlandsLog\.js/g;
+        if(args.indexOf('isbd')!= -1){
+            vtrHtml = vtrHtml.replace(rgx,'https://www.sunlands.wang/wtm.js');
+            vtrHtml = vtrHtml.replace(regs,'https://b.bdstatic.com/searchbox/icms/searchbox/js/swan-1.6.0.js');
+        }else{
+            vtrHtml = vtrHtml.replace(rgx,'https://img.sunlands.wang/wtm.js');
+            vtrHtml = vtrHtml.replace(regs,'https://res.wx.qq.com/open/js/jweixin-1.3.2.js');
+        }
         fs.writeFileSync('./index.html',vtrHtml,'utf8');
-        console.log(process.argv);
-         
     });
 })
